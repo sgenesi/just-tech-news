004b88f1eddd0db19cb6dc45844fce9a275d8d3c
Staci Genesi <sgenesi@hotmail.com>
2021-02-21 14:13:01 -0700

    create dashboard for logged in users

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,19 +1,4 @@
-const { TestScheduler } = require("jest");
 const { format_date, format_plural, format_url } = require('../utils/helpers');
-
-test('format_date() returns a date string', () => {
-    const date = new Date('2020-03-20 16:12:03');
-
-    expect(format_date(date)).toBe('3/20/2020');
-});
-
-test('format_plural() returns a pluralized word', () => {
-    const word1 = format_plural('tiger', 2);
-    const word2 = format_plural('lion', 1);
-
-    expect(word1).toBe('tigers');
-    expect(word2).toBe('lion');
-});
 
 test('format_url() returns a simplified url string', () => {
     const url1 = format_url('http://test.com/page/1');
@@ -23,4 +8,18 @@ test('format_url() returns a simplified url string', () => {
     expect(url1).toBe('test.com');
     expect(url2).toBe('coolstuff.com');
     expect(url3).toBe('google.com');
+});
+
+test('format_plural() returns a pluralized word', () => {
+    const word1 = format_plural('tiger', 1);
+    const word2 = format_plural('lion', 2);
+
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lions');
+});
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+
+    expect(format_date(date)).toBe('3/20/2020');
 });
